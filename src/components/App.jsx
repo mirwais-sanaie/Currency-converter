6b7b67d0a12ffdfd1daa3d775fc31cca18e1aa08
Mirwais <mirwais.sanaie@gmail.com>
2025-01-02 23:50:18 +0430

    UI completed .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,41 @@
+import ReactDropdown from "react-dropdown";
+import styles from "./App.module.css";
+import { HiSwitchHorizontal } from "react-icons/hi";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className={styles.App}>
+      {/* heading  */}
+      <div className={styles.heading}>
+        <h1>Currency converter</h1>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h3>Amount</h3>
+          <input type="Enter the amount" />
+        </div>
+
+        <div className={styles.middle}>
+          <h3>From</h3>
+          <ReactDropdown placeholder="form" />
+        </div>
+
+        <div className={styles.switchClass}>
+          <HiSwitchHorizontal size="30px" />
+        </div>
+
+        <div className={styles.right}>
+          <h3>TO</h3>
+          <ReactDropdown placeholder="form" />
+        </div>
+      </div>
+
+      <div className={styles.result}>
+        <button>Convert</button>
+        <h2>Converted Amount:</h2>
+        <p>X</p>
+      </div>
     </div>
   );
 }
