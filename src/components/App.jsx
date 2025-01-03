64b6d191ab7e69da9816f699fbd4dae7cd703147
Mirwais <mirwais.sanaie@gmail.com>
2025-01-03 22:44:30 +0430

    input accept just number.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,43 @@
 import ReactDropdown from "react-dropdown";
 import styles from "./App.module.css";
 import { HiSwitchHorizontal } from "react-icons/hi";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [input, setInput] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [from, setFrom] = useState(0);
+  const [to, setTo] = useState(0);
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    if (value === "" || /^[0-9]+$/.test(value)) {
+      setInput(value);
+    }
+  };
+
+  useEffect(
+    function () {
+      async function getData() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${input}`
+          );
+          const data = await res.json();
+          console.log(data);
+        } catch (err) {
+          setError(err.message);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      getData();
+    },
+    [from, to, input]
+  );
+
   return (
     <div className={styles.App}>
       {/* heading  */}
@@ -13,7 +48,11 @@ function App() {
       <div className={styles.container}>
         <div className={styles.left}>
           <h3>Amount</h3>
-          <input type="Enter the amount" />
+          <input
+            type="Enter the amount"
+            value={input}
+            onChange={(e) => handleInputChange(e)}
+          />
         </div>
 
         <div className={styles.middle}>
